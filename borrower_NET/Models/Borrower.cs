e9e00ac7023e6600eb62ef0cb0c4f288972c7f12
anisha256 <59680341+anisha256@users.noreply.github.com>
2022-09-19 02:49:04 +0545

    add models

---
+++ borrower_NET/Models/Borrower.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace borrower_NET.Models
+{
+    public class Borrower
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Address { get; set; }
+        public string MobileNumber { get; set; }
+        public int FundAmount { get; set; }
+        public string FundPurpose { get; set; }
+        public string BusinessType { get; set; }
+    }
+}
---
+++ borrower_NET/Models/User.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace borrower_NET.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+        public string Username { get; set; }
+        public string MobileNumber { get; set; }
+        public string EmailId { get; set; }
+        public string Pincode { get; set; }
+    }
+}
